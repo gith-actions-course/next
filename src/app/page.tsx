65169b9a785cc69c86589e780e9b6393d90363c3
ann-wrst <annwrst@gmail.com>
2025-05-23 11:30:14 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
-
-export default function Home() {
+import flagsmith from "@/utlis/flagsmith";
+export const revalidate = 0;
+export default async function Home() {
+  const flags = await flagsmith.getEnvironmentFlags();
   return (
     <main className="main">
       <div>
         <h2>Hello World</h2>
       </div>
       <Link href="/about">About</Link>
+      <br />
+      {flags.isFeatureEnabled('search') &&
+        <input placeholder="search" />}
     </main>
   );
 }
---
+++ src/utlis/flagsmith.ts
@@ -0,0 +1,6 @@
+import { Flagsmith } from 'flagsmith-nodejs';
+
+const flagsmith = new Flagsmith({
+  environmentKey: process.env.FLAGSMITH_KEY as string
+});
+export default flagsmith;
